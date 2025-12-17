3d8a807a0ac6ff922f70dafa457f70688452604f
Kavita Suresh Pandey <persuadify@gmail.com>
2025-12-17 15:22:12 +0530

    Update db.php

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,11 +1,12 @@
 <?php
-$host="localhost";
-$db="asha_matrimonial";
-$user="DB_USER";
-$pass="DB_PASS";
+$host = "localhost";
+$db   = "tuxcepgg_ashamatrimonial";
+$user = "tuxcepgg_ashauser";
+$pass = "APNA_REAL_DATABASE_PASSWORD_YAHAN_DALO";
 
-$conn = new mysqli($host,$user,$pass,$db);
-if($conn->connect_error){
-  die("DB Connection Failed");
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("DB Error: " . $conn->connect_error);
 }
 ?>
